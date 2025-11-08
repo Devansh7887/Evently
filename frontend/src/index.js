b4dd388ee4374a7f53a9daaad3a26ef2be6116c3
Devansh7887 <devanshagrawal01813@gmail.com>
2025-11-08 20:47:16 +0530

    fix: add node engine

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthContext'; // Ise hum abhi banayenge
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
